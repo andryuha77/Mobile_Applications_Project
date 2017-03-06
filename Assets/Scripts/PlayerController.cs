82a37361ee49c263c5595bc11833c2026e0d9265
Andrei Petruk <andryuha1977@gmail.com>
2017-03-06 23:11:17 +0000

    Play Area,Collectable Objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	//reference varible
+	private Rigidbody rb;
+
+	//used for initialisation
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	//input phisics location
+	void FixedUpdate ()
+	{
+		//input select word ctrl+' for reserch in unity docummentation
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
