76a58b5b63c83d1473c142c84736f275438dbd06
andryuha77 <andryuha1977@gmail.com>
2017-03-19 19:34:36 +0000

    Collecting Objects, Displaying the Score and Text

---
+++ Extend_Roll_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	//reference varible
+	private Rigidbody rb;
+	private int count;
+
+	//used for initialisation
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	//input phisics location
+	void FixedUpdate ()
+	{
+		//input select word ctrl+' for reserch in unity docummentation
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Pick Up"))
+		{
+			//activate or deactivate gameobject
+			other.gameObject.SetActive (false);
+			//increment cont
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 10)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
