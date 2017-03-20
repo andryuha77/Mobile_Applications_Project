e77d049940084f9a633869c1e993d83cfffbd61f
andryuha77 <andryuha1977@gmail.com>
2017-03-20 01:34:13 +0000

    Jumping and bouncing

--- Extend_Roll_Ball/Assets/Scripts/PlayerController.cs
+++ Extend_Roll_Ball/Assets/Scripts/PlayerController.cs
@@ -30,6 +30,16 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	// jumping functionaliity
+		{
+			if (Input.GetKeyDown ("space") && GetComponent<Rigidbody>().transform.position.y <= 0.6250001f) {
+				Vector3 jump = new Vector3 (0.0f, 200.0f, 0.0f);
+
+				GetComponent<Rigidbody>().AddForce (jump);
+			}
+		}
+
 		rb.AddForce (movement * speed);
 	}
 
