0d64643d7015a6a6ae9e7c218d35726d2bd8a85a
andryuha77 <andryuha1977@gmail.com>
2017-03-25 21:26:21 +0000

    Maze and more objects

--- Extend_Roll_Ball/Assets/Scripts/PlayerController.cs
+++ Extend_Roll_Ball/Assets/Scripts/PlayerController.cs
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour {
 	void SetCountText ()
 	{
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 10)
+		if (count >= 17)
 		{
 			winText.text = "You Win!";
 		}
